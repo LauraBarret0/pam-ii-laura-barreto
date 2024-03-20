9a331f43f9b55871dbc00a8e71d1e6be69bd6d05
Laura Barreto <barreto.laura023@gmail.com>
2024-03-20 08:31:17 -0300

    Criação do primeiro arquivo javascript

---
+++ aula01/metodosevariaveis.js
@@ -0,0 +1,15 @@
+// variavel global
+var variavel1 = "Olá";
+
+// varialvel local - Sempre usar
+let variavel2 = 3;
+
+// Constante
+const variavel3 = 8;
+
+// Métodos já vem configurados 
+console.log(
+    "Olá mundo!"
+);
+
+console.log(variavel1, "mundo!");
